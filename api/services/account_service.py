00a9d927cab9583514a202b3d1d8d4e903ac86ad
lisiyu <lisiyu@haizhi.com>
2025-03-21 17:36:53 +0800

    # [Starry] directory bug fix 修复新建用户默认创建两个角色

--- api/services/account_service.py
+++ api/services/account_service.py
@@ -952,11 +952,12 @@ class RegisterService:
             if open_id is not None and provider is not None:
                 AccountService.link_account_integrate(provider, open_id, account)
 
-            if FeatureService.get_system_features().is_allow_create_workspace and create_workspace_required:
-                tenant = TenantService.create_tenant(f"{account.name}'s Workspace")
-                TenantService.create_tenant_member(tenant, account, role="owner")
-                account.current_tenant = tenant
-                tenant_was_created.send(tenant)
+            # [Starry] directory user
+            # if FeatureService.get_system_features().is_allow_create_workspace and create_workspace_required:
+            #     tenant = TenantService.create_tenant(f"{account.name}'s Workspace")
+            #     TenantService.create_tenant_member(tenant, account, role="owner")
+            #     account.current_tenant = tenant
+            #     tenant_was_created.send(tenant)
 
             db.session.commit()
         except WorkSpaceNotAllowedCreateError:
