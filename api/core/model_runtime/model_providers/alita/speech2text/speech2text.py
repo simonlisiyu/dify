10cfdf4fa4484930d5360363a8192cd321098a1a
lisiyu <lisiyu@haizhi.com>
2025-03-31 15:32:44 +0800

    # [Starry] directory llm 增加alita的模型支持embedding、rerank、speech、tts的非标准格式url

--- api/core/model_runtime/model_providers/alita/speech2text/speech2text.py
+++ api/core/model_runtime/model_providers/alita/speech2text/speech2text.py
@@ -33,7 +33,13 @@ class AlitaSpeech2text(Speech2TextModel):
         :return: text for given audio file
         """
         
-        url = str(URL(credentials['server_url']) / "v1/audio/transcriptions")
+        server_url_standard = credentials.get('server_url_standard', 'false')
+        is_standard = server_url_standard.lower() == 'true'
+        if is_standard:
+            url = str(URL(credentials['server_url']) / "v1/audio/transcriptions")
+        else:
+            url = str(URL(credentials['server_url']))
+
         data = {"model": model}
         files = {"file": file}
 
